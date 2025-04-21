28f02139252734f84a655e27c533be29798efc2c
davyd <davydedivaldo@gmail.com>
2025-04-21 19:24:31 -0300

    estilização do pots e implementando os artigo e aprende sobre props

--- src/componentes/Artigos/index.js
+++ src/componentes/Artigos/index.js
@@ -3,10 +3,19 @@ import Post from '../Posts';
 function Artigo(){
     return(
         <>
-        <h1>componente artigo</h1>
-        <Post></Post>
-        </>
+        <section className='container'>
 
+        <h1>componente artigo</h1>
+        <div className='container-artigo'>
+            <Post subtitle='Tecnologia' title='O guia definitivo do blog'></Post>
+            <Post subtitle='Fotografia' title='Quais as melhores câmeras em 2025'></Post>
+            <Post subtitle='Cinema' title='Os 10 melhores filmes de 2024'></Post>
+        </div>
+         
+        </section>
+
+        </>
+        
     );
 }
 
--- src/componentes/Posts/index.js
+++ src/componentes/Posts/index.js
@@ -1,9 +1,23 @@
 
-function Post(){
+function Post(props){
     return(
-        <h1>
-            componente post
-        </h1>
+        <>
+        <div className='post'>
+            <div className='img-post'></div>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p className=''>Minim dolor est cillum non irure proident sunt voluptate amet consequat ea. Anim elit nostrud velit cupidatat minim non amet tempor ut consequat et. Consectetur sit nulla amet amet eiusmod ut aliquip enim laborum.</p>
+            <div className='flex container-perfil'>
+                <div className='img-perfil'></div>
+                <div className='descript-perfil'>
+                    <h6>Autor do post</h6>
+                    <p>Abril, 2025 - 8 min leitura</p>
+                </div>
+
+            </div> 
+        </div>
+
+        </>
     );
 }
 export default Post;
